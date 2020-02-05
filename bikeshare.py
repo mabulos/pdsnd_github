8507a0402c76771d315c5060d6362da8b62dab65
mabulos2012 <mabulos2012@gmail.com>
2020-02-05 13:13:05 +0200

    additional changes-bikeshare file and ReadMe

--- bikeshare.py
+++ bikeshare.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import datetime
-
+# working on version control - addition changes
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
